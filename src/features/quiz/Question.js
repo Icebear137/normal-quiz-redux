d12279209dec420954425fdf36b812d2ee846897
LeDuy_Icebear <dangleduy137@gmail.com>
2023-12-13 11:49:16 +0700

    fix: display question order

--- src/features/quiz/Question.js
+++ src/features/quiz/Question.js
@@ -1,14 +1,16 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 
 const Question = ({ question, handleAnswerOptionClick }) => {
   
   console.log(question)
+  const { currentQuestion } = useSelector((state) => state.quiz);
 
   return (
     <>
       <div className="question-section">
                 <div className="question-count">
-                    <span>Question {question?.questionID}</span>
+                    <span>Question {currentQuestion + 1}</span>
                 </div>
                 <div className="question-text">{question?.questionText}</div>
         </div>
